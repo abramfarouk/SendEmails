79b2e5032be08ae2883c6b9f26cf04e9ada416e3
abram farouk <abramfarouk62@gmail.com>
2024-09-17 11:12:07 +0300

    AddMessage

--- SendEmails/Dtos/WelcomeRequestDto.cs
+++ SendEmails/Dtos/WelcomeRequestDto.cs
@@ -4,10 +4,10 @@ namespace SendEmails.Dtos
 {
     public class WelcomeRequestDto
     {
-        [Required]
+        [Required(ErrorMessage = "Plz enter UserName")]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Plz enter Email")]
         public string Email { get; set; }
     }
 }
